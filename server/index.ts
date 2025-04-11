99bc5d5d11900e936a28c225a08ec7c5017ad843
anshujod <anshujod@users.noreply.github.com>
2025-04-11 16:20:22 +0530

    fix: update configurations for Railway deployment

--- server/index.ts
+++ server/index.ts
@@ -56,15 +56,12 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5001
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5002;
+  const PORT = process.env.PORT || 5000;
   server.listen({
-    port,
+    port: PORT,
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
-    log(`serving on port ${port}`);
+    log(`serving on port ${PORT}`);
   });
 })();
